872018e7df23e9ee3c5586aeb09656e6d0b0e6b6
ackenney <ackenney@gmail.com>
2023-05-03 17:21:51 -0400

    added files

---
+++ Test1.asm
@@ -0,0 +1,102 @@
+;Program Description: This program calucates the mean and variance from the data in RAW
+;Program 1.asm
+INCLUDE Irvine32.inc
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess proto, dwExitCode:dword
+
+.data
+	; Declaring variable with the values given in the program guide
+	RAW	    WORD 10, 12, 8, 17, 9, 22 , 18 , 11 , 23 , 7 , 30 , 22 , 19 , 6 , 7 
+	SQUARE	WORD LENGTHOF RAW dup(?)
+	str1    BYTE "MEAN ",0
+	str2	BYTE "VAR  ",0
+	MEAN	WORD ?
+	VAR     WORD ?
+
+.code
+main proc
+	 
+; Setting registers for loop 1
+mov edi,OFFSET RAW		    ; EDI = address of RAW
+mov ecx,LENGTHOF RAW		; initialize loop counter
+mov eax,0					; sum = 0
+
+
+; Getting the Sum of the array  from this loop the sum will be in ax
+L1:					; The beginning of loop
+add ax,[edi]		; Addition
+add edi, TYPE RAW	; Pointing to next element
+loop L1
+
+
+; Division to get the mean. The mean will be in ax
+mov dx, 0			; Setting dx to 0
+mov cx,LENGTHOF RAW ; cx = n
+div cx				; = sum / n
+mov MEAN, ax		; setting mean
+
+
+;Printing mean:
+mov edx,OFFSET str1
+call WriteString
+mov dx, MEAN
+call writedec
+call crlf
+
+
+; Setting registers for loop 2
+mov esi, OFFSET SQUARE 
+mov edi,OFFSET RAW		    ; EDI = address of RAW
+mov ecx,LENGTHOF RAW		; initialize loop counter
+mov eax,0					; sum = 0
+
+L2:					
+
+; Setting registers
+mov eax,0
+mov ebx, 0
+mov ax,[edi]	
+mov bx, MEAN
+
+; Evaluating expression
+sub eax, ebx	; x - mean
+mov ebx,eax		; eax = x ebx = x
+mul eax			; x*x
+
+mov [esi], eax ; Moving square to array
+
+add esi, TYPE SQUARE  ; Moving to next element 
+add edi, TYPE RAW	  ; Moving to next element
+loop L2
+
+; Setting registers for loop 3
+mov eax, 0
+mov edi, OFFSET SQUARE 
+mov ecx, LENGTHOF SQUARE 
+
+; This loop sums each square
+L3:
+	add ax, [edi]			; Adding to the sum
+	add edi, TYPE SQUARE	; Moving to next value
+loop L3
+
+
+; Division by n to get the variance. The variance will be in ax
+mov dx, 0
+mov cx,LENGTHOF RAW
+div cx
+mov VAR, ax
+
+; Printing Variance
+mov edx,OFFSET str2
+call WriteString
+mov dx,VAR
+call writedec
+call crlf
+
+
+	invoke ExitProcess,0
+main endp
+end main
---
+++ Test2.asm
@@ -0,0 +1,83 @@
+; Program Description: This program gives the user a letter grade from the values 0 - 100
+; Program 2.asm
+
+
+INCLUDE Irvine32.inc
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess proto, dwExitCode:dword
+
+.data
+	; Declaring data for program
+	str1 BYTE "Enter Value: ", 0
+	str2 BYTE "Letter Grade: ", 0
+	grade WORD "A", "B" , "C" , "D" , "E" , 0
+	
+
+.code
+main proc
+	
+	; ecx is set to 10 so the output would be compact and still show each if statement working correctly
+	; It can be changed to test any number of values
+	mov ecx, 10 ; loop counter = 10 (Testing ten values)
+
+	; If only one value needed to be tested this input loop could be removed
+	
+	INPUT:
+	
+	; Printing header for input
+	mov edx,OFFSET str1
+	call WriteString
+
+	; Getting value
+	call readint
+
+	; Printing header for letter grade
+	mov edx,OFFSET str2
+	call WriteString
+
+	; setting edx for output later
+	mov edx, OFFSET grade
+
+	cmp al,90
+	jb L1						; Go to L1 if al < 90		
+								; No Displacement needed for 'A'
+	jmp L5						; Go to end(L5) if al >= 90
+
+	L1:
+	cmp al,80
+	jb L2						; Go to L2 if al < 80
+	add edx, 1* type grade		; Displacement to move to 'B' 
+	jmp L5						; if 90 > al >= 80 go to L5
+	
+	L2:
+	cmp al,70
+	jb L3						; Go to L3 if al < 70
+	add edx, 2* type grade		; Displacement to move to 'C' in the array
+	jmp L5						; if 80 > al >= 70 go to L5
+
+	L3:
+	cmp al,60
+	jb L4						; Go to L4 if al < 60
+	add edx, 3* type grade		; Displacement to move to 'D' in the array
+	jmp L5						; if 70 > al >= 60 go to L5
+	
+	L4: 
+	; if al < 60
+	add edx, 4* type grade		; Displacement to move to 'E' in the array
+
+	L5: 
+	
+
+	; Print the letter grade
+	call writestring 
+	call crlf
+	call crlf
+
+	loop INPUT ; End of input loop
+
+
+	invoke ExitProcess,0
+main endp
+end main
